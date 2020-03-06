36c0679f28c1d6c8e36730fe35c3c2d95057d2f3
jinyongnan810 <919142075@qq.com>
2020-03-06 14:45:54 +0900

    0.4 implement babel

---
+++ mymemo/frontend/src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { Provider } from "react-redux";
+import {
+    HashRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from "react-router-dom"
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+                Hello world
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+// export default App
