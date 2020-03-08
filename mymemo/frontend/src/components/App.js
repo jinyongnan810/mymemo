f7e7bcb3efe471e1f479972e8d0f3ff17b5dd7de
金勇男 <kin@kinisaonoMacBook-puro.local>
2020-03-08 09:59:41 +0900

    0.4 implement babel fix

--- mymemo/frontend/src/components/App.js
+++ mymemo/frontend/src/components/App.js
@@ -1,16 +1,15 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-import { Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-import { Provider } from "react-redux";
-import {
-    HashRouter as Router,
-    Route,
-    Switch,
-    Redirect
-} from "react-router-dom"
+// import { Provider as AlertProvider } from "react-alert";
+// import AlertTemplate from "react-alert-template-basic";
+// import { Provider } from "react-redux";
+// import {
+//     HashRouter as Router,
+//     Route,
+//     Switch,
+//     Redirect
+// } from "react-router-dom"
 
-import React, { Component } from 'react'
 
 export class App extends Component {
     componentDidMount() {
